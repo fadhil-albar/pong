bbbfa1c5777e3fd9269d7a33507376b78c885d7b
fadhil-albar <fadhilalbar111@gmail.com>
2020-04-15 17:52:50 +0700

    random

---
+++ Assets/balikgan.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class balikgan : MonoBehaviour
+{
+    public void balik()
+    {
+        SceneManager.LoadScene("menu");
+    }
+}
